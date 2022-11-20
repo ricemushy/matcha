081ad56f79e4c9dee18e3fd9c8c656e673f8803a
ricemushy <136066772+ricemushy@users.noreply.github.com>
2022-11-20 12:29:45 +0300

    feat(fetcher): scrape manga directory

--- web/shared/fetcher.ts
+++ web/shared/fetcher.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import axios from "axios";
+import { load } from "cheerio";
 
-export class Fetcher {
+class Fetcher {
   public baseUrl = "http://www.mangahere.cc";
   public serviceBaseUrl = "https://api.consumet.org";
 
@@ -26,4 +28,32 @@ export class Fetcher {
     );
     return data;
   }
+
+  public async getMangaDirectory(page: number) {
+    const result: Record<string, any> = {};
+
+    try {
+      const { data } = await axios.get(`${this.baseUrl}/directory/${page}.htm`);
+      const $ = load(data);
+
+      result.hasNextPage =
+        $("div.pager-list-left > a.active").next().text() !== ">";
+
+      result.results = $("div.container > div > div > ul > li")
+        .map((_i, el) => ({
+          id: $(el).find("a").attr("href")?.split("/")[2]!,
+          title: $(el).find("p.manga-list-1-item-title > a").text(),
+          subTitle: $(el).find("p.manga-list-1-item-subtitle > a").text(),
+          image: $(el).find("a > img").attr("src"),
+          headerForImage: { Referer: this.baseUrl },
+        }))
+        .get();
+
+      return result;
+    } catch (err) {
+      throw new Error((err as Error).message);
+    }
+  }
 }
+
+export default new Fetcher();
