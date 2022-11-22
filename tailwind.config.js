5e48deadffe88f654b72222bbe65e9d8a8bbb52d
ricemushy <136066772+ricemushy@users.noreply.github.com>
2022-11-22 09:07:18 +0300

    fix(styling): remove default    styling override
    hard-coded styling to the button component instead

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 const production = !process.env.ROLLUP_WATCH;
 module.exports = {
   future: {
     purgeLayersByDefault: true,
     removeDeprecatedGapUtilities: true,
   },
+
   plugins: [],
+  theme: {
+    extend: {
+      backgroundSize: {
+        fullPercent: "100% 100%",
+      },
+    },
+  },
   content: ["./web/**/*.svelte"],
 };
--- web/components/Button.svelte
+++ web/components/Button.svelte
@@ -4,8 +4,10 @@
   let variantStyle: string;
 
   const variantType = {
-    primary: "from-green-400 via-green-500 to-green-600",
-    secondary: "from-red-400 via-red-500 to-red-600",
+    primary:
+      "from-green-400 via-green-500 to-green-600 focus:ring-green-300 dark:focus:ring-green-800 ",
+    secondary:
+      "from-red-400 via-red-500 to-red-600 focus:ring-red-300 dark:focus:ring-red-800",
   };
 
   switch (variant) {
@@ -24,7 +26,7 @@
 
 <main>
   <button
-    class="text-white bg-gradient-to-r {style} hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 my-2"
+    class="text-white w-full bg-gradient-to-r {style} hover:bg-gradient-to-br focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-3.5 text-center mr-2 mb-2 my-2"
     type="button"
     class:variant
     on:click
