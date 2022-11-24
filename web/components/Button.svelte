d681ac3d6d29ccd1c65369e69992f12174441fb8
ricemushy <136066772+ricemushy@users.noreply.github.com>
2022-11-24 23:14:38 +0300

    fix(button): remove margins

--- web/components/Button.svelte
+++ web/components/Button.svelte
@@ -26,7 +26,7 @@
 
 <main>
   <button
-    class="text-white w-full bg-gradient-to-r {style} hover:bg-gradient-to-br focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-3.5 text-center mr-2 mb-2 my-2"
+    class="text-white w-full bg-gradient-to-r {style} hover:bg-gradient-to-br focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-3.5 text-center mr-2"
     type="button"
     class:variant
     on:click
